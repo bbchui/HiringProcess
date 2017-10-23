cff09747d1c554d26697851df5b52434bc4a96e4
Brandon Chui <bbchui325@gmail.com>
2017-10-23 16:10:18 -0700

    stageCreator, stats, create

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 let fs = require('fs');
-let text = fs.readFileSync("./input.txt", "utf-8")
-let textByLine = text.split('\n')
+let text = fs.readFileSync("./input.txt", "utf-8");
+let textByLine = text.split('\n');
 
-console.log(textByLine);
+// console.log(textByLine);
+let outputText = [];
+
+let applicants = [];
+
+let stages = {};
+
+// Incase DEFINE not in txt file
+DEFAULT_STAGES = {"ManualReview" : 0,
+                  "PhoneInterview" : 0,
+                  "BackgroundCheck" : 0,
+                  "DocumentSigning" : 0}
+
+
+const create = function(name) {
+  if (!applicants.includes(name)) {
+    applicants.push(name);
+    outputText.push(`CREATE ${name}`);
+  } else {
+    outputText.push("Duplicate Applicant");
+  }
+}
+
+const stageCreator = function(defineLine) {
+  let str = defineLine.split(" ");
+  for (var i = 1; i < str.length; i++) {
+    stages[str[i]] = 0;
+  }
+}
+
+const stats = function(stage) {
+  let record = [];
+  Object.keys(stage).forEach(key => {
+    record.push(key);
+    record.push(stage[key]);
+  });
+  outputText.push(record.join(" "));
+}
+
+
+create("hi")
+create("hi")
+stageCreator("DEFINE ManualReview BackgroundCheck DocumentSigning")
+stats(stages)
+console.log(outputText);
